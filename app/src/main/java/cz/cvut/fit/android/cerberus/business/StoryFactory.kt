5f06af3491df5e47c4bb25edd396d19f0b3753bc
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-05 14:38:29 +0100

    Add link to Hard Landing story node to factory

--- app/src/main/java/cz/cvut/fit/android/cerberus/business/StoryFactory.kt
+++ app/src/main/java/cz/cvut/fit/android/cerberus/business/StoryFactory.kt
@@ -3,12 +3,14 @@ package cz.cvut.fit.android.cerberus.business
 import cz.cvut.fit.android.cerberus.structures.story.StoryNode
 import cz.cvut.fit.android.cerberus.structures.story.UnknownNode
 import cz.cvut.fit.android.cerberus.structures.story.chapters.first.Beginning
+import cz.cvut.fit.android.cerberus.structures.story.chapters.first.HardLanding
 
 object StoryFactory {
 
     fun getStoryNode(ID: Long, previousID: Long): StoryNode {
         return when (ID) {
             0L -> Beginning()
+            1L -> HardLanding(previousID)
             else -> UnknownNode(previousID)
         }
     }
