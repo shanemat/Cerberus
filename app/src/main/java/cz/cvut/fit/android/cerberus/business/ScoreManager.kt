276095ddff08a8291f07cdcb9dc845ac2450bfcb
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-05 22:24:25 +0100

    Add dummy manager keeping track of which games is already scored

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/business/ScoreManager.kt
@@ -0,0 +1,11 @@
+package cz.cvut.fit.android.cerberus.business
+
+import cz.cvut.fit.android.cerberus.structures.enums.PlayerRole
+
+object ScoreManager {
+
+    fun isGameScored(role: PlayerRole): Boolean {
+        // TODO implement actual check based on DB value
+        return false
+    }
+}
