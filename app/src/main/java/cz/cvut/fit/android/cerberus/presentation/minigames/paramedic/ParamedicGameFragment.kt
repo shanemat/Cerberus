1d0714eb5c22adf4708728534b1fb75c7a427a5d
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-05 18:43:17 +0100

    Add dummy paramedic game's fragment
    Also include its layout

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/presentation/minigames/paramedic/ParamedicGameFragment.kt
@@ -0,0 +1,21 @@
+package cz.cvut.fit.android.cerberus.presentation.minigames.paramedic
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cz.cvut.fit.android.cerberus.R
+
+class ParamedicGameFragment internal constructor() : Fragment() {
+
+    companion object Creator {
+        fun newInstance(): ParamedicGameFragment {
+            return ParamedicGameFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.f_games_paramedic, container, false)
+    }
+}
