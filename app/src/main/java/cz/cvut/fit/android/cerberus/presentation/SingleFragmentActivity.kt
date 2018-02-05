cfd21b9fd7ddaa5fa777f10bb5677bcb84ad85d1
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-05 16:59:08 +0100

    Add abstract activity providing ways to hold single fragment

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/presentation/SingleFragmentActivity.kt
@@ -0,0 +1,32 @@
+package cz.cvut.fit.android.cerberus.presentation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import cz.cvut.fit.android.cerberus.R
+
+abstract class SingleFragmentActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        wireUpView()
+        attachFragment()
+    }
+
+    private fun wireUpView() {
+        setContentView(R.layout.a_single_fragment)
+    }
+
+    private fun attachFragment() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+
+        if (fragment == null) {
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.fragmentContainer, createFragment())
+                    .commit()
+        }
+    }
+
+    protected abstract fun createFragment(): Fragment
+}
