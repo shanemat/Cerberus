d8c87eb9a8de77b31952a7f5f288244becc779f0
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-05 18:52:32 +0100

    Add dummy archaeologist's game fragment
    Also include its layout

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/presentation/minigames/archaeologist/ArchaeologistGameFragment.kt
@@ -0,0 +1,21 @@
+package cz.cvut.fit.android.cerberus.presentation.minigames.archaeologist
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cz.cvut.fit.android.cerberus.R
+
+class ArchaeologistGameFragment internal constructor() : Fragment() {
+
+    companion object Creator {
+        fun newInstance(): ArchaeologistGameFragment {
+            return ArchaeologistGameFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.f_games_archaeologist, container, false)
+    }
+}
