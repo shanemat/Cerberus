95f9ca260a108a24451dc06ce8a236201d23617c
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-10 20:36:32 +0100

    Add class enabling use of parametrized singleton in Kotlin
    Big thank to @Christophe Beyls at Medium! You rock, man! :))

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/structures/prototype/SingletonHolder.kt
@@ -0,0 +1,27 @@
+package cz.cvut.fit.android.cerberus.structures.prototype
+
+open class SingletonHolder<out T, in A>(creator: (A) -> T) {
+
+    private var creator: ((A) -> T)? = creator
+
+    @Volatile private var instance: T? = null
+
+    fun getInstance(arg: A): T {
+        val i = instance
+        if (i != null) {
+            return i
+        }
+
+        return synchronized(this) {
+            val i2 = instance
+            if (i2 != null) {
+                i2
+            } else {
+                val created = creator!!(arg)
+                instance = created
+                creator = null
+                created
+            }
+        }
+    }
+}
