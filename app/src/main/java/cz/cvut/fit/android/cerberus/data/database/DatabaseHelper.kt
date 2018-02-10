01a059b1be5739f06a3f68ade01a8bcd4bc80bf1
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-10 20:39:13 +0100

    Add class for handling database access

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/data/database/DatabaseHelper.kt
@@ -0,0 +1,35 @@
+package cz.cvut.fit.android.cerberus.data.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import cz.cvut.fit.android.cerberus.data.database.prototype.Table
+import cz.cvut.fit.android.cerberus.structures.prototype.SingletonHolder
+
+class DatabaseHelper private constructor (context: Context)
+    : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object : SingletonHolder<DatabaseHelper, Context>(::DatabaseHelper) {
+        const val DATABASE_NAME = "Cerberus-DB"
+        const val DATABASE_VERSION = 1
+    }
+
+    override fun onCreate(database: SQLiteDatabase?) {
+        val tables = getTables()
+        for (table in tables) {
+            table.initialize(database)
+        }
+    }
+
+    override fun onUpgrade(database: SQLiteDatabase?, oldVersion: Int, currentVersion: Int) {
+        // intentionally left blank -  add handling for different versions if necessary
+    }
+
+    private fun getTables(): ArrayList<Table> {
+        val tables = ArrayList<Table>()
+
+        // add all database tables
+
+        return tables
+    }
+}
