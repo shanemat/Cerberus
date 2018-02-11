23b0378431ba41e7d92996276048ec780c2aa2c5
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-11 13:15:43 +0100

    Add implementation of name fetching for StoryNode

--- app/src/main/java/cz/cvut/fit/android/cerberus/structures/story/node/StoryNode.kt
+++ app/src/main/java/cz/cvut/fit/android/cerberus/structures/story/node/StoryNode.kt
@@ -1,6 +1,7 @@
 package cz.cvut.fit.android.cerberus.structures.story.node
 
 import android.content.Context
+import cz.cvut.fit.android.cerberus.business.ManagerFactory
 import cz.cvut.fit.android.cerberus.structures.enums.PlayerRole
 import cz.cvut.fit.android.cerberus.structures.answers.StoryAnswer
 
@@ -20,7 +21,6 @@ abstract class StoryNode(links: StoryLinks,
 
     private val textResourceID = resources.textResourceID
     private val textRole = roles.textRole
-    private val baseName = getBaseName()
 
     var answers: ArrayList<StoryAnswer> = ArrayList()
         private set
@@ -30,12 +30,15 @@ abstract class StoryNode(links: StoryLinks,
     }
 
     fun getText(context: Context): String {
+        val baseName = getBaseName(context)
         return context.resources.getString(textResourceID, baseName)
     }
 
-    private fun getBaseName(): String {
-        // TODO add base name retrieval - based on TEXT ROLE!!!
-        return "Unknown"
+    private fun getBaseName(context: Context): String {
+        val manager = ManagerFactory.getPlayerManager()
+        val player = manager.getPlayer(context, textRole)
+
+        return player?.name ?: "---"
     }
 
     data class StoryLinks(val ID: Long,
