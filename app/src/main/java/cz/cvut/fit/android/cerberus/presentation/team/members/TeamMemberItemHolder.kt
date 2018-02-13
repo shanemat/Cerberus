69b94d59d61849422b020adc774f242724f58609
Matěj Sháněl <shanemat@fit.cvut.cz>
2018-02-13 09:32:05 +0100

    Add item holder for team members
    Including its layout

---
+++ app/src/main/java/cz/cvut/fit/android/cerberus/presentation/team/members/TeamMemberItemHolder.kt
@@ -0,0 +1,10 @@
+package cz.cvut.fit.android.cerberus.presentation.team.members
+
+import android.view.View
+import android.widget.TextView
+import cz.cvut.fit.android.cerberus.R
+
+class TeamMemberItemHolder(convertView: View) {
+
+    var textView: TextView = convertView.findViewById(R.id.iTeamMemberTextView)
+}
